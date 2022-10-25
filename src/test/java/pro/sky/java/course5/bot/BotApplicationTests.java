2e66ea8216e7f8b7e2ce70c944ab3ec366daa9a2
shulga_ea <shulga23@gmail.com>
2022-10-25 13:59:38 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course5/bot/BotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course5.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
